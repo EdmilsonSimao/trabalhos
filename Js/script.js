8c06c3f1bd749e759e171845acaf6bca673a7473
Edmar Jr <151618243+EdmilsonSimao@users.noreply.github.com>
2024-01-10 22:42:44 +0100

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.scroll', {
+    rotate:{x:0, y:100,z:0},
+    duration: 2000 });
+sr.reveal('.scroll-1', {
+    rotate:{x:100, y:0,z:0},
+    duration: 2000 });
+sr.reveal('.scroll-2', {duration: 2000 });
+sr.reveal('.scroll-3', {
+    rotate:{x:0, y:100,z:10},
+    duration: 2000 });
